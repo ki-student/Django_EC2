8b5f6c7dd09266d426b4d0d059bcd71d8e787d5b
ki-student <kiwonjune@naver.com>
2025-07-16 18:43:08 +0900

    modify setting

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(x!sf28@1fx6l-o&1ienw6di5_5@^2swhh(&08t=%p%gwgp&mn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
